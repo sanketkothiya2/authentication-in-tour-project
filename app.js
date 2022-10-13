2f2c91cd41fb864b14925df6fda258ff66f89b29
sanket_kothiya <48357133+Bimal2614@users.noreply.github.com>
2022-10-13 16:34:25 +0530

    final updation done

--- app.js
+++ app.js
@@ -25,22 +25,25 @@ if (process.env.NODE_ENV === 'development') {
 
 // Limit requests from same API
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: '100kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
 // Data sanitization against XSS
+//  dont take malicious data like html.javascript code and many more
 app.use(xss());
 
 // Prevent parameter pollution
+// we basically ues 
+// which data alo in query string 
 app.use(
   hpp({
     whitelist: [
